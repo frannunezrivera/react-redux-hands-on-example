56e0ae13d11615dc8c978d2ab18861f45282d662
Fran Nunez <fran.nunez@rakuten.com>
2017-02-07 10:22:44 +0900

    Set up the store and provider

--- src/index.js
+++ src/index.js
@@ -3,7 +3,32 @@ import ReactDOM from 'react-dom';
 import App from './views/App/App';
 import './index.css';
 
+import createLogger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+
+import {getAllItems} from './actions'
+
+import reducer from './reducers'
+
+const middleware = [thunk];
+if(process.env.NODE_ENV !== 'production') {
+    middleware.push(createLogger());
+}
+
+
+const store = createStore(
+    reducer,
+    applyMiddleware(...middleware)
+);
+
+store.dispatch(getAllItems());
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
